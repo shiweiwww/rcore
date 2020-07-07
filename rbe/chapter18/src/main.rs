78b924938595bdfce01807aadc8bdbb20e17c0ac
shiweixxx <swxxxa@163.com>
2020-07-07 16:22:55 +0800

    day schedule

---
+++ rbe/chapter18/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+#[test]
+fn test1() {
+
+
+    let opt:Option<&str>=Some("13");
+    println!("opt = {}",opt.unwrap());
+
+    match opt{
+        Some("13")=>println!("opt is match ={}",opt.unwrap()),
+        _ => {}
+    }
+
+
+
+}
