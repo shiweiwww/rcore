0b2a8d7d2da7534d23f5eb6637fff8c33a1e5e35
shiweixxx <swxxxa@163.com>
2020-07-07 16:52:20 +0800

    day schedule

---
+++ rbe/chapter19/src/main.rs
@@ -0,0 +1,58 @@
+fn main() {
+    println!("Hello, world!");
+}
+#[test]
+fn test1() {
+    #[derive(Debug)]
+    struct point{
+        x:i32,
+        y:i32
+    }
+    let p = point{x:2,y:4};
+    println!("p={:?}",p);
+    let b = Box::new(p);
+    println!("b.x={}",(*b).x);
+
+}
+#[test]
+fn test2() {
+    use std::collections::{HashMap,HashSet};
+
+    let mut d = HashMap::new();
+    d.insert("k1",101);
+    d.insert("k2",102);
+    d.insert("k3",103);
+    println!("d={:?}",d);
+
+    let mut set = HashSet::new();
+    set.insert(4);
+    set.insert(5);
+    set.insert(6);
+    println!("set = {:?}",set);
+
+
+    let mut set1 = HashSet::new();
+    set1.insert(3);
+    set1.insert(2);
+    set1.insert(1);
+    println!("set1 = {:?}",set1);
+    let mut m = set.union(&set1).collect::<Vec<&i32>>();
+    println!("m = {:?}",m[0]);
+
+}
+#[test]
+fn test3() {
+
+
+    use std::rc::Rc;
+    let rc_a = Rc::new("a share string");
+    println!("rc_a share count = {}",Rc::strong_count(&rc_a));
+    {
+
+        let rc_b = Rc::clone(&rc_a);
+        println!("rc_b share count = {}",Rc::strong_count(&rc_b));
+        println!("rc_a share count = {}",Rc::strong_count(&rc_a));
+
+    }
+    println!("rc_a share count = {}",Rc::strong_count(&rc_a));
+}
